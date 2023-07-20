f0c48b69f3d1cc44a309334a462a176dca550e7e
Bertrand Servin <bertrand.servin@inrae.fr>
2023-07-20 13:56:39 +0200

    Use fasphase and pgenlib external packages rather than keeping them in the source tree Move packaging to pyproject.toml

--- hapflk/utils.py
+++ hapflk/utils.py
@@ -1,7 +1,8 @@
 import datetime as dt
 
+
 class Stepper(object):
-    def __init__(self, prefix=''):
+    def __init__(self, prefix=""):
         """
 
         :type prefix: basestring
@@ -9,23 +10,29 @@ class Stepper(object):
         self.start = dt.datetime.now()
         self.ncalls = 0
         self.prefix = prefix
-        self.write(msg="Start @ "+dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        self.write(msg="Start @ " + dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     @staticmethod
     def formatTD(td):
         hours = td.seconds // 3600
         minutes = (td.seconds % 3600) // 60
         seconds = td.seconds % 60
-        return '%02d:%02d:%02d' % (hours, minutes, seconds)
+        return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
-    def new(self, msg='Step'):
+    def new(self, msg="Step"):
         self.ncalls += 1
-        print(self.prefix, self.ncalls, ". [", self.formatTD(dt.datetime.now()-self.start), "] ", msg)
-        return dt.datetime.now()-self.start
+        print(
+            self.prefix,
+            self.ncalls,
+            ". [",
+            self.formatTD(dt.datetime.now() - self.start),
+            "] ",
+            msg,
+        )
+        return dt.datetime.now() - self.start
 
     def write(self, msg):
-        print ((10+len(self.prefix))*' '+msg)
+        print((10 + len(self.prefix)) * " " + msg)
 
     def end(self):
-        return self.new(msg="The End @ "+dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
+        return self.new(msg="The End @ " + dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
